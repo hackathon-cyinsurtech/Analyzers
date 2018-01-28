cd3e08efee49cd25b8f2bbab6f3521c59bbd1fe8
earist07 <earist07@cs.ucy.ac.cy>
2018-01-28 15:07:45 +0200

    Changes to user's car details

--- api/objects/user.php
+++ api/objects/user.php
@@ -33,5 +33,32 @@ class User{
 		return $stmt;
 	}
 
+	function addCarInsurances($arr){
+		$query = "INSERT INTO `user_car_details` (`id`, `reg_no`, `make`, `model`, `color`, `type`, `cc`, `year`, `passengers`,
+																							`est_value`, `acc`, `own`, `health_prob`, `have`, `accident`, `criminal`, `user_id`)
+				  		VALUES (NULL, '" . $arr["reg_no"] . "' , '" . $arr["make"] . "' , '" . $arr["model"] . "' , '" . $arr["color"] . "' , '" . $arr["type"]
+							. "' , '" . $arr["cc"] . "' , '" . $arr["year"] . "' , '" . $arr["passengers"] . "' , '" . $arr["est_value"] . "' , '" . $arr["acc"]
+							 . "' , '" . $arr["own"] . "' , '" . $arr["health_prob"] . "' , '" . $arr["have"] . "' , '" . $arr["accident"]
+							  . "' , '" . $arr["criminal"] . "' , '" . $arr["user_id"]  . "')";
+		$stmt = $this->conn->prepare($query);
+		if ($stmt->execute()!=NULL){
+			$query = "SELECT max(id) id FROM user_car_details WHERE user_id = " . $arr["user_id"];
+			$stmt = $this->conn->prepare($query);
+			$stmt->execute();
+			$row = $stmt->fetch(PDO::FETCH_ASSOC);
+			$id = $row["id"];
+			return $id;
+		}
+		return NULL;
+	}
+
+	function readCarInsurances($userid){
+		$query = "SELECT * FROM user_car_details WHERE user_id = ?";
+		$stmt = $this->conn->prepare($query);
+		$stmt->bindParam(1,$userid);
+		$stmt->execute();
+		return $stmt;
+	}
+
 }
 ?>
--- api/user/readCarDetails.php
+++ api/user/readCarDetails.php
@@ -4,20 +4,17 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 include_once "../config/connect_db.php";
-include_once "../objects/user_car_details.php";
+include_once "../objects/user.php";
 
 $database = new Database();
 $db = $database->connect();
 
 // initialize object
-$user_car_details = new UserCarDetails($db);
+$user = new User($db);
 
 $id = isset($_GET["id"]) ? $_GET["id"] : die();
-$stmt = $user_car_details->read($id);
+$stmt = $user->readCarInsurances($id);
 $num = $stmt->rowCount();
-
-`id`, `reg_no`, `make`, `model`, `color`, `type`, `cc`, `year`, `passengers`,
-`est_value`, `acc`, `own`, `health_prob`, `have`, `accident`, `criminal`, `user_id`
 
 // check if more than 0 record found
 if($num>0){
